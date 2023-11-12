8cb4c1e730a26b55595e309d89b542df968e87de
AymenKabine2003 <147838962+AymenKabine2003@users.noreply.github.com>
2023-11-12 18:15:39 +0100

    Revert "client model"
    This reverts commit 59df0d55b6468464cfe3fb3fe1d59c665da721c5.

--- TPS_Kahoot_Client/src/main/Main.java
+++ TPS_Kahoot_Client/src/main/Main.java
@@ -2,7 +2,6 @@ package main;
 
 import java.awt.EventQueue;
 
-import model.Client;
 import view.Client_GUI;
 
 public class Main {
@@ -11,7 +10,6 @@ public class Main {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Client client = new Client();
 					Client_GUI frame = new Client_GUI();
 					frame.setVisible(true);
 				} catch (Exception e) {
