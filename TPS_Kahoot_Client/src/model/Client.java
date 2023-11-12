f1cf9f4d64de29a1d4e334b146e0988019573a18
AymenKabine2003 <147838962+AymenKabine2003@users.noreply.github.com>
2023-11-12 18:13:58 +0100

    Revert "aggiunta panello client invio richiesta di connessione"
    This reverts commit 145d983ebb257e5759d91b9f58f8ba269b017fa7.

--- TPS_Kahoot_Client/src/model/Client.java
+++ TPS_Kahoot_Client/src/model/Client.java
@@ -1,12 +1,9 @@
 package model;
 
 import java.io.*; 
-import java.net.*;
-
-import view.Client_GUI; 
+import java.net.*; 
 
 public class Client { 
-	Client_GUI gui = new Client_GUI();
 	private Socket connessione; 
 	private BufferedReader dalServer; 
 	private PrintStream alServer; // Scrive bytes mentre PrintWriter scrive caratteri 
@@ -14,23 +11,12 @@ public class Client {
 	public Client() { 
 		BufferedReader tastiera = new BufferedReader(new InputStreamReader(System.in)); 
 		try { 
-			
 			System.out.println("Inserire l'indirizzo del server"); 
-			String indirizzo = gui.ipserver; 
+			String indirizzo = tastiera.readLine(); 
 			connessione = new Socket(indirizzo, 20000); 
 			dalServer = new BufferedReader(new InputStreamReader(connessione.getInputStream()));
 			alServer = new PrintStream(connessione.getOutputStream()); 
-			gui.panelRichiestaClient.setVisible(false);
 		} catch (IOException e) { e.printStackTrace(); } 
 	}
-	public void connectToServer(String serverAddress) {
-        try {
-            connessione = new Socket(serverAddress, 20000);
-            dalServer = new BufferedReader(new InputStreamReader(connessione.getInputStream()));
-            alServer = new PrintStream(connessione.getOutputStream());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
  
 }
